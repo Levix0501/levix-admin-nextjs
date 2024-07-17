a66e1b255fea183056a5b13274d0ea85f9efcad0
Levix <1176891456@qq.com>
2024-07-17 15:18:29 +0800

    chore: lint

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import BrandColorPicker from '@core/components/BrandColorPicker';
 import { appConfig } from '@config/appConfig';
 
 export default function Home() {
-  return (
-    <main className="h-screen flex justify-center pt-10">
-      <div className="space-y-4">
-        <div>
-          <p>Logo</p>
-          {appConfig.logo}
-        </div>
+	return (
+		<main className="h-screen flex justify-center pt-10">
+			<div className="space-y-4">
+				<div>
+					<p>Logo</p>
+					{appConfig.logo}
+				</div>
 
-        <div>
-          <p>主题</p>
-          <ThemeChanger />
-        </div>
+				<div>
+					<p>主题</p>
+					<ThemeChanger />
+				</div>
 
-        <div>
-          <p>品牌色</p>
-          <BrandColorPicker />
-        </div>
-      </div>
-    </main>
-  );
+				<div>
+					<p>品牌色</p>
+					<BrandColorPicker />
+				</div>
+			</div>
+		</main>
+	);
 }
