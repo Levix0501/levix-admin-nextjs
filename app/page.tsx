7525e479a9be193172fb25bdc88eb3d94e163ba2
Levix <1176891456@qq.com>
2024-07-26 10:18:20 +0800

    feat: 新增 github 地址和 Vue3 版本地址

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import ThemeChanger from '@core/components/theme-changer/ThemeChanger';
 import BrandColorPicker from '@core/components/BrandColorPicker';
 import { appConfig } from '@config/appConfig';
+import Link from 'next/link';
+import { LuGithub } from 'react-icons/lu';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<main className="h-screen flex justify-center pt-10">
 			<div className="space-y-4">
+				<div className="flex space-x-4">
+					<Link
+						href="https://github.com/Levix0501/levix-admin-nextjs"
+						target="_blank"
+					>
+						<LuGithub size={24} />
+					</Link>
+
+					<Link href="https://vue-admin.fecoder.cn" target="_blank">
+						Vue3 版本
+					</Link>
+				</div>
+
 				<div>
 					<p>Logo</p>
 					{appConfig.logo}
